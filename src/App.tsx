55be19f6230e9e39d8c1c00e4b46791c87408a4b
NK2121 <nahomnk11@gmail.com>
2024-08-07 17:41:50 -0500

    Starting Pages including routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-//import React from 'react';
+import AuthLayout from './_auth/AuthLayout';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import { Home } from './_root/pages';
+import RootLayout from './_root/RootLayout';
 import './globals.css';
+import{ Routes,Route} from 'react-router-dom'
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
+    <main className="flex h-screen">
+        <Routes>
+            {/* public routes*/}
+            <Route element={<AuthLayout />}>
+                <Route path="/sign-in" element = {< SignInForm/>} />
+                <Route path="/sign-up" element = {< SignUpForm/>} />
+            </Route>
+            
+            {/* private routes*/}
+            <Route element={<RootLayout />}>
+                <Route index element={<Home/>}/>
+            </Route>
+        </Routes>
+    </main>
   );
 };
 
